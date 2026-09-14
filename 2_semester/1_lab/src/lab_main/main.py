b29b7cb939db1cc82ccbed7e2e14767f7c78fac7
BobasB <bugil.bogdan@gmail.com>
2026-09-14 11:50:10 +0300

    почали setUP

--- 2_semester/1_lab/src/lab_main/main.py
+++ 2_semester/1_lab/src/lab_main/main.py
@@ -10,6 +10,15 @@ def check_passed_parameter(p: str = None):
 def count_passed_parameters(*args):
     return len(args)
 
+class MainLab:
+    GLOBALS = [1, 2]
+    def __init__(self):
+        self.name = "Main Lab"
+
+    def return_numbers(self):
+        for number in self.GLOBALS:
+            yield number
+
 
 if __name__ == "__main__":
     main()
--- 2_semester/1_lab/tests/test_unit.py
+++ 2_semester/1_lab/tests/test_unit.py
@@ -1,9 +1,11 @@
 import unittest
-from lab_main.main import check_passed_parameter, count_passed_parameters
+from lab_main.main import check_passed_parameter, count_passed_parameters, MainLab
 
-
+###
 class TestCountPassedParameters(unittest.TestCase):
     def test_count_passed_parameters(self):
+        """
+        Тестуємо передану функцію count_passed_parameters з різними кількостями параметрів."""
         self.assertEqual(
             count_passed_parameters(1, 2, 3),
             3,
@@ -11,6 +13,32 @@ class TestCountPassedParameters(unittest.TestCase):
         )
         self.assertEqual(count_passed_parameters("a", "b"), 2)
         self.assertEqual(count_passed_parameters(), 0)
+
+###
+class TestMainLabConstructor(unittest.TestCase):
+    def setUp(self):
+        """
+        Метод setUp виконується перед кожним тестом.
+        """
+        print("*** Виконується setUp ***")
+        self.main_lab = MainLab()
+
+    def tearDown(self):
+        """
+        Метод tearDown виконується після кожного тесту.
+        """
+        print("*** Виконується tearDown ***")
+        del self.main_lab
+    
+    def test_main_lab_constructor(self):
+        """
+        Тест може падати через незалежні умови.
+        """
+        self.assertEqual(self.main_lab.name, "Main Lab", "Очікується, що атрибут 'name' буде 'Main Lab' після створення об'єкта MainLab")
+        self.assertTrue(False, "Навмисно провалений тест для перевірки механізму тестування")
+
+    def test_object_attributes(self):
+        self.assertTrue(hasattr(self.main_lab, "name"), "Об'єкт повинен мати атрибут 'name'")
 
 
 if __name__ == "__main__":
